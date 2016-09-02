49603e30533647e67db5d1666c19a0b3a91233a7
James Thompson <james@projector.com>
2016-09-02 08:38:24 -0400

    working, but not thoroughly test 1st v Datastore

--- src/Data/Gogol/DatastoreEntity.hs
+++ src/Data/Gogol/DatastoreEntity.hs
@@ -5,17 +5,36 @@
 {-# LANGUAGE TypeFamilies        #-}
 {-# LANGUAGE TypeOperators       #-}
 
-module Data.Gogol.DatastoreEntity where
+module Data.Gogol.DatastoreEntity (
+    EntityTransform(..)
+  , DatastoreEntity(..)
+  , _ToDatastoreEntity
+  , _FromDatastoreEntity
+  ) where
 
 import           Control.Applicative      ((<|>))
-import           Control.Lens             hiding (from, to)
+import           Control.Lens             (Getter, at, over, view, (&), (.~),
+                                           (?~), (^.), (^?), _1, _Just, _head)
+import qualified Control.Lens.Getter      as L
+import           Data.ByteString          (ByteString)
 import qualified Data.HashMap.Lazy        as HM
+import           Data.Int                 (Int32, Int64)
+import           Data.List.NonEmpty       (NonEmpty, fromList, toList)
+import           Data.Maybe               (fromMaybe)
 import           Data.Text                (Text, pack)
+import           Data.Time.Clock          (UTCTime)
+import           GHC.Float                (double2Float, float2Double)
 import           GHC.Generics
-import           Network.Google.Datastore (Entity, EntityProperties, Value,
-                                           eProperties, entity,
-                                           entityProperties, epAddtional,
-                                           vEntityValue, value)
+import           Network.Google.Datastore (Entity, Key, LatLng, Value,
+                                           arrayValue, avValues, eKey,
+                                           eProperties, eProperties, entity,
+                                           entityProperties, epAddtional, kPath,
+                                           key, pathElement, peKind,
+                                           vArrayValue, vBlobValue,
+                                           vBooleanValue, vDoubleValue,
+                                           vEntityValue, vGeoPointValue,
+                                           vIntegerValue, vKeyValue,
+                                           vStringValue, vTimestampValue, value)
 
 data EntityTransform
   = EntityC Text [(Text, Value)]
@@ -80,6 +99,7 @@ instance (GDatastoreGet f, Constructor c) => GDatastoreGet (C1 c f) where
   gEntGet (V v)          = do props <- v^.vEntityValue
                               ep    <- props^.eProperties
                               gEntGet (EntityP . HM.toList $ ep^.epAddtional)
+  gEntGet None           = Nothing
 
 instance (GDatastorePut f, Selector c) => GDatastorePut (S1 c f) where
   gEntPut s@(M1 x) = case gEntPut x of
@@ -95,10 +115,111 @@ instance (GDatastoreGet f, Selector c) => GDatastoreGet (S1 c f) where
   gEntGet (EntityP xs) = gEntGet =<< V <$> HM.fromList xs^.at s
     where s = pack $ selName (undefined :: t c f p)
   gEntGet (V v)        = M1 <$> gEntGet (V v)
+  gEntGet _            = Nothing
 
 instance DatastoreEntity a => GDatastorePut (K1 i a) where
   gEntPut = toEntity . unK1
 
 instance DatastoreEntity a => GDatastoreGet (K1 i a) where
   gEntGet = fmap K1 . fromEntity
+
+instance DatastoreEntity Bool where
+  toEntity b = V $ value & vBooleanValue ?~ b
+  fromEntity (V v) = v^.vBooleanValue
+  fromEntity _     = Nothing
+
+instance DatastoreEntity Int where
+  toEntity i = V $ value & vIntegerValue ?~ fromIntegral i
+  fromEntity (V v) = fromIntegral <$> v^.vIntegerValue
+  fromEntity _     = Nothing
+
+instance DatastoreEntity Integer where
+  toEntity i = V $ value & vIntegerValue ?~ fromIntegral i
+  fromEntity (V v) = fromIntegral <$> v^.vIntegerValue
+  fromEntity _     = Nothing
+
+instance DatastoreEntity Int32 where
+  toEntity i = V $ value & vIntegerValue ?~ fromIntegral i
+  fromEntity (V v) = fromIntegral <$> v^.vIntegerValue
+  fromEntity _     = Nothing
+
+instance DatastoreEntity Int64 where
+  toEntity i = V $ value & vIntegerValue ?~ i
+  fromEntity (V v) = v^.vIntegerValue
+  fromEntity _     = Nothing
+
+instance DatastoreEntity Float where
+  toEntity f = V $ value & vDoubleValue ?~ float2Double f
+  fromEntity (V v) = double2Float <$> v^.vDoubleValue
+  fromEntity _     = Nothing
+
+instance DatastoreEntity Double where
+  toEntity d = V $ value & vDoubleValue ?~ d
+  fromEntity (V v) = v^.vDoubleValue
+  fromEntity _     = Nothing
+
+instance DatastoreEntity ByteString where
+  toEntity b = V $ value & vBlobValue ?~ b
+  fromEntity (V v) = v^.vBlobValue
+  fromEntity _     = Nothing
+
+instance DatastoreEntity Text where
+  toEntity x = V $ value & vStringValue ?~ x
+  fromEntity (V v) = v^.vStringValue
+  fromEntity _     = Nothing
+
+instance DatastoreEntity Key where
+  toEntity k = V $ value & vKeyValue ?~ k
+  fromEntity (V v) = v^.vKeyValue
+  fromEntity _     = Nothing
+
+instance DatastoreEntity UTCTime where
+  toEntity t = V $ value & vTimestampValue ?~ t
+  fromEntity (V v) = v^.vTimestampValue
+  fromEntity _     = Nothing
+
+instance DatastoreEntity LatLng where
+  toEntity l = V $ value & vGeoPointValue ?~ l
+  fromEntity (V v) = v^.vGeoPointValue
+  fromEntity _     = Nothing
+
+instance DatastoreEntity a => DatastoreEntity (NonEmpty a) where
+  toEntity xs = V $ value & vArrayValue ?~ (arrayValue & avValues .~
+                                             (toList xs >>= indiv))
+    where indiv x = case toEntity x of
+            V v -> [v]
+            _   -> []
+  fromEntity (V v) = do vals   <- v^?vArrayValue._Just.avValues
+                        parsed <- traverse fromEntity $ V <$> vals
+                        return $ fromList parsed
+  fromEntity _     = Nothing
+
+instance DatastoreEntity a => DatastoreEntity [a] where
+  toEntity xs = V $ value & vArrayValue ?~ (arrayValue & avValues .~
+                                             (xs >>= indiv))
+    where indiv x = case toEntity x of
+            V v -> [v]
+            _   -> []
+  fromEntity (V v) = do vals <- v^?vArrayValue._Just.avValues
+                        traverse fromEntity $ V <$> vals
+  fromEntity _     = Nothing
+
+instance DatastoreEntity a => DatastoreEntity (Maybe a) where
+  toEntity (Just x) = toEntity x
+  toEntity Nothing  = V value
+  fromEntity z@(V _) = (pure <$> fromEntity z) <|> pure Nothing
+  fromEntity _       = Nothing
+
+_ToDatastoreEntity :: DatastoreEntity a => Getter a (Maybe Entity)
+_ToDatastoreEntity = L.to (toEntity' . toEntity)
+  where toEntity' (EntityC k params) =
+          pure $ entity & eKey ?~ (key & kPath .~ [pathElement & peKind ?~ k])
+                        & eProperties ?~ entityProperties (HM.fromList params)
+        toEntity' _                  = Nothing
+
+_FromDatastoreEntity :: DatastoreEntity a => Getter Entity (Maybe a)
+_FromDatastoreEntity = L.to (fromEntity . toTransform)
+  where toTransform e =
+          fromMaybe None $ EntityC <$> (view kPath <$> e^.eKey)^._Just._head.peKind
+                                   <*> (HM.toList <$> (e^?eProperties._Just.epAddtional))
 

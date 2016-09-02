e32b2385f94cec79b0dd16a3311892dbe1707ff4
James Thompson <james@projector.com>
2016-09-01 21:29:03 -0400

    WIP: working recursive Entity -> a parsing
    - Sum type encoding works ok
    - Nullable value parsing not yet working, probably something dumb

--- src/Data/Gogol/DatastoreEntity.hs
+++ src/Data/Gogol/DatastoreEntity.hs
@@ -1,66 +1,104 @@
-{-# LANGUAGE DefaultSignatures #-}
-{-# LANGUAGE FlexibleContexts  #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE TypeFamilies      #-}
-{-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE DefaultSignatures   #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE FlexibleInstances   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE TypeOperators       #-}
 
 module Data.Gogol.DatastoreEntity where
 
+import           Control.Applicative      ((<|>))
 import           Control.Lens             hiding (from, to)
 import qualified Data.HashMap.Lazy        as HM
 import           Data.Text                (Text, pack)
 import           GHC.Generics
-import           Network.Google.Datastore (Value, eProperties, entity,
-                                           entityProperties, vEntityValue,
-                                           value)
+import           Network.Google.Datastore (Entity, EntityProperties, Value,
+                                           eProperties, entity,
+                                           entityProperties, epAddtional,
+                                           vEntityValue, value)
 
 data EntityTransform
-  = Entity Text [(Text, Value)]
+  = EntityC Text [(Text, Value)]
   | EntityP [(Text, Value)]
   | V Value
   | None
   deriving (Eq, Show)
 
 class DatastoreEntity a where
-  entity' :: a -> EntityTransform
-  default entity' :: (Generic a, GDatastore (Rep a)) => a -> EntityTransform
-  entity' = gentity' . from
+  toEntity :: a -> EntityTransform
+  fromEntity :: EntityTransform -> Maybe a
 
-class GDatastore f where
-  gentity' :: f a -> EntityTransform
+  default toEntity :: (Generic a, GDatastorePut (Rep a)) => a -> EntityTransform
+  toEntity = gEntPut . from
 
-instance GDatastore U1 where
-  gentity' _ = None
+  default fromEntity ::(Generic a, GDatastoreGet (Rep a)) => EntityTransform -> Maybe a
+  fromEntity x = to <$> gEntGet x
 
-instance (GDatastore a, GDatastore b) => GDatastore (a :+: b) where
-  gentity' (L1 x) = gentity' x
-  gentity' (R1 x) = gentity' x
+class GDatastorePut f where
+  gEntPut :: f a -> EntityTransform
 
-instance (GDatastore a, GDatastore b) => GDatastore (a :*: b) where
-  gentity' (a :*: b) = case (gentity' a, gentity' b) of
+class GDatastoreGet f where
+  gEntGet :: EntityTransform -> Maybe (f a)
+
+instance GDatastorePut U1 where
+  gEntPut _ = None
+
+instance GDatastoreGet U1 where
+  gEntGet _ = Nothing
+
+instance (GDatastorePut a, GDatastorePut b) => GDatastorePut (a :+: b) where
+  gEntPut (L1 x) = gEntPut x
+  gEntPut (R1 x) = gEntPut x
+
+instance (GDatastoreGet a, GDatastoreGet b) => GDatastoreGet (a :+: b) where
+  gEntGet e = L1 <$> gEntGet e <|> R1 <$> gEntGet e
+
+instance (GDatastorePut a, GDatastorePut b) => GDatastorePut (a :*: b) where
+  gEntPut (a :*: b) = case (gEntPut a, gEntPut b) of
     (EntityP xs, EntityP ys)  -> EntityP $ xs ++ ys
     _                         -> None
 
-instance GDatastore f => GDatastore (M1 D d f) where
-  gentity' = gentity' . unM1
+instance (GDatastoreGet a, GDatastoreGet b) => GDatastoreGet (a :*: b) where
+  gEntGet e = (:*:) <$> gEntGet e <*> gEntGet e
 
-instance (GDatastore f, Constructor c) => GDatastore (M1 C c f) where
-  gentity' x
-    | conIsRecord x = case gentity' $ unM1 x of
-        EntityP xs -> Entity (pack (conName x)) xs
+instance GDatastorePut f => GDatastorePut (D1 d f) where
+  gEntPut = gEntPut . unM1
+
+instance GDatastoreGet f => GDatastoreGet (D1 d f) where
+  gEntGet = fmap M1 . gEntGet
+
+instance (GDatastorePut f, Constructor c) => GDatastorePut (C1 c f) where
+  gEntPut x
+    | conIsRecord x = case gEntPut $ unM1 x of
+        EntityP xs -> EntityC (pack (conName x)) xs
         v          -> v
-    | otherwise = gentity' $ unM1 x
+    | otherwise = gEntPut $ unM1 x
 
-instance (GDatastore f, Selector c) => GDatastore (M1 S c f) where
-  gentity' s@(M1 x) = case gentity' x of
-      V v         -> EntityP [(pack (selName s), v)]
-      EntityP xs  -> EntityP $ over _1 (const (pack (selName s))) <$> xs
-      Entity _ xs -> EntityP [(pack (selName s), mkEntityV xs)]
-      _           -> None
+instance (GDatastoreGet f, Constructor c) => GDatastoreGet (C1 c f) where
+  gEntGet (EntityC _ xs) = M1 <$> gEntGet (EntityP xs)
+  gEntGet (EntityP xs)   = M1 <$> gEntGet (EntityP xs)
+  gEntGet (V v)          = do props <- v^.vEntityValue
+                              ep    <- props^.eProperties
+                              gEntGet (EntityP . HM.toList $ ep^.epAddtional)
+
+instance (GDatastorePut f, Selector c) => GDatastorePut (S1 c f) where
+  gEntPut s@(M1 x) = case gEntPut x of
+      V v           -> EntityP [(pack (selName s), v)]
+      EntityP xs    -> EntityP $ over _1 (const (pack (selName s))) <$> xs
+      EntityC _  xs -> EntityP [(pack (selName s), mkEntityV xs)]
+      _             -> None
     where mkEntityV xs = value & vEntityValue ?~
                            (entity & eProperties ?~
                               entityProperties (HM.fromList xs))
 
-instance DatastoreEntity a => GDatastore (K1 i a) where
-  gentity' = entity' . unK1
+instance (GDatastoreGet f, Selector c) => GDatastoreGet (S1 c f) where
+  gEntGet (EntityP xs) = gEntGet =<< V <$> HM.fromList xs^.at s
+    where s = pack $ selName (undefined :: t c f p)
+  gEntGet (V v)        = M1 <$> gEntGet (V v)
+
+instance DatastoreEntity a => GDatastorePut (K1 i a) where
+  gEntPut = toEntity . unK1
+
+instance DatastoreEntity a => GDatastoreGet (K1 i a) where
+  gEntGet = fmap K1 . fromEntity
 
